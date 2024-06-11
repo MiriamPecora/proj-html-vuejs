98b1e78f57450283f3982200c562655d24187abf
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-06-11 16:23:15 +0200

    [ADD] AppLearnMore

---
+++ src/components/AppLearnMore.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+    name:"learn more",
+    props: [
+        'imagePath',
+        'title',
+    ],
+    computed: {
+        getImagesPath(){
+            // prende in input il nome del file
+            return new URL("../assets/img/home/"+this.imagePath, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<template>
+<div class="learn-more">
+    <img :src="getImagesPath" :alt="`${title} car img`">
+    <h6>{{ title }}</h6>
+    <button class="btn">LEARN MORE</button>
+</div>
+</template>
+
+<style>
+
+</style>
