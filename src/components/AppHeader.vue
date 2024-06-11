8b3ff343dbc366200091ed26f6dd6431f1983cb1
michelealiffi <michelealiffi890@gmail.com>
2024-06-11 11:19:30 +0200

    Update header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,52 @@ export default {
 
 <template>
 <header>
-    <h1>Header</h1>
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid px-5 mx-5 my-3 fw-bold">
+          <a class="navbar-brand" href="#">
+            <img src="/src/assets/img/header/avada-drivers-logo-1x.webp" alt="">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse mx-5 pt-3" id="navbarNavDropdown">
+            <ul class="navbar-nav mx-5">
+              <li class="nav-item mx-1">
+                <a class="nav-link active" aria-current="page" href="#">HOME</a>
+              </li>
+              <li class="nav-item mx-1">
+                <a class="nav-link" href="#">ABOUT</a>
+              </li>
+              <li class="nav-item mx-1">
+                <a class="nav-link" href="#">PRICES</a>
+              </li>
+              <li class="nav-item dropdown mx-1">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  COURSES
+                  <span class="">NEW</span>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Intensive</a></li>
+                  <li><a class="dropdown-item" href="#">Pass Plus</a></li>
+                  <li><a class="dropdown-item" href="#">Instructor</a></li>
+                </ul>
+              </li>
+              <li class="nav-item mx-1">
+                <a class="nav-link" href="#">LOCATIONS</a>
+              </li>
+              <li class="nav-item mx-1">
+                <a class="nav-link" href="#">BLOG</a>
+              </li>
+              <li class="nav-item mx-1">
+                <a class="nav-link" href="#">
+                    <button class="btn">BOOK NOW</button>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 </header>
 </template>
 
@@ -14,5 +59,9 @@ export default {
 header{
     position: absolute;
     top: 0;
+}
+
+nav {
+    background-color: #3333338C;
 }
 </style>
