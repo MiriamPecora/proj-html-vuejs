0dc695744542405880c496f9d3f3006b0c667e5d
michelealiffi <michelealiffi890@gmail.com>
2024-06-12 18:04:39 +0200

    [UPD] style

--- src/components/AppStartLearning.vue
+++ src/components/AppStartLearning.vue
@@ -1,4 +1,10 @@
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPhone)
+
 export default {
     name: 'Start Learning',
     methods: {
@@ -12,6 +18,17 @@ export default {
 
 <template>
 <img :src="getImagesPath('background-car.png')" alt="background car">
+<div class="container d-flex justify-content-between">
+    <div>
+        <h1 class="text-dark">Start Learning to Drive Today</h1>
+        <p class="text-dark">Give us a call to schedule your first driving lesson</p>
+    </div>
+    <div>
+        <p class="text-dark fs-2">
+            <font-awesome-icon class="me-3" icon="fa-solid fa-phone"/> 1-800-555-555
+        </p>
+    </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
