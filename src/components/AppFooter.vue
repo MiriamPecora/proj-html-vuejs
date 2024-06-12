112f5ca15b0de6a8c6fb8d2c5d12385b6cc70e9d
MiriamPecora <miriampecora97@gmail.com>
2024-06-12 15:25:10 +0200

    [UPD] Updated top section of the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,9 @@
 <script>
+    
     export default {
         name: 'Footer'
     }
+    
 </script>
 
 <template>
@@ -19,6 +21,65 @@
             </div>
         </div>
         
+        <div id="top_footer">
+            <div class="container-lg d-flex justify-content-between py-5">
+                <div class="row">
+                    <div class="col">
+                        <div class="content">
+                            <img class="pb-4" src="../assets/img/header/avada-drivers-logo-1x.webp" alt="">
+                            <p class="pb-5 lh-lg">Avada Driving School is the right choice for experienced drivers looking for a refresher course, or the brand new driver wanting to pass first time.</p>
+                            <button class="btn rounded-5 btn-outline-secondary px-4 py-2 fw-semibold">MORE ABOUT US</button>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content mt-3">
+                            <h4 class="pb-4">Driving Courses</h4>
+                            <ul class="d-flex flex-column gap-3">
+                                <li>
+                                    <a href="/">Basic Driving Course</a>
+                                </li>
+                                <li>
+                                    <a href="/">Pass Plus Driving Course</a>
+                                </li>
+                                <li>
+                                    <a href="/">Intensive Driving Course</a>
+                                </li>
+                                <li>
+                                    <a href="/">Instructors Training</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content mt-3">
+                            <h4 class="pb-4">Contact Details</h4>
+                            <ul class="d-flex flex-column gap-3">
+                                <li>
+                                    <span>50 North Main Street <br>
+                                    New York, NY 98765</span>
+                                </li>
+                                <li>
+                                    <span>Toll Free: 1-800-555-555</span>
+                                </li>
+                                <li>
+                                    <span>info@your-domain.com</span>
+                                </li>
+                                <li>
+                                    <span>Mon to Sat: 8 am – 5 pm</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="content mt-3">
+                            <h4 class="pb-4">Book Free Class</h4>
+                            <img class="pb-4" src="../assets/img/footer/blogpost-ten.jpg" alt="">
+                            <button id="greenbtn" class="btn rounded-5 px-4 py-2 fw-semibold text-white" type="submit">BOOK NOW</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </footer>
 </template>
@@ -26,6 +87,11 @@
 <style lang="scss" scoped>
 
     @use '../assets/scss/partials/variables' as *;
+
+    a {
+        text-decoration: none;
+        color: $color-text ;
+    }
 
     #newsletter {
         background-image: url("../assets/img/footer/newsletter_background.jpg");
@@ -40,4 +106,20 @@
         }
     }
 
+    #top_footer {
+        background-image: url("../assets/img/footer/footer-background-color.jpg");
+        background-size:cover;
+
+        img {
+            width: 250px;
+        }
+    }
+
+    #greenbtn{
+        background-color: $color-main;
+    }
+
+    // .col {
+    //     border: solid black 1px;
+    // }
 </style>
