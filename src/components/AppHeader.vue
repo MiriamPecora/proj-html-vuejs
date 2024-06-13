4429b9e5d7b0186045aa0a38caad413fd91c2abc
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-06-13 12:34:17 +0200

    [UPD] changed navbar layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,51 +20,84 @@ export default {
             </p>
         </div>
     </div>
-    <nav class="navbar navbar-expand-lg ps-1">
-        <div class="container-lg my-3 fw-semibold">
-          <a class="navbar-brand" href="#">
-            <img src="/src/assets/img/header/avada-drivers-logo-1x.webp" alt="">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse pt-3 justify-content-end" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item mx-3 ms-5">
-                <a class="nav-link active text-white" aria-current="page" href="#">HOME</a>
-              </li>
-              <li class="nav-item mx-3">
-                <a class="nav-link text-white" href="../pages/PageAbout.vue">ABOUT</a>
-              </li>
-              <li class="nav-item mx-3">
-                <a class="nav-link text-white" href="#">PRICES</a>
-              </li>
-              <li class="nav-item dropdown mx-3">
-                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  COURSES
-                  <span class="rounded-1 p-1 mx-1 btn span-nav bg-new fw-semibold text-white">NEW</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Intensive</a></li>
-                  <li><a class="dropdown-item" href="#">Pass Plus</a></li>
-                  <li><a class="dropdown-item" href="#">Instructor</a></li>
-                </ul>
-              </li>
-              <li class="nav-item mx-3">
-                <a class="nav-link text-white" href="#">LOCATIONS</a>
-              </li>
-              <li class="nav-item mx-3">
-                <a class="nav-link text-white" href="#">BLOG</a>
-              </li>
-              <li class="nav-item mx-3">
-                <a class="nav-link" href="/src/pages/PageForm.vue">
-                    <button class="btn bg-new rounded-5 px-4 py-2 fw-semibold text-white" type="submit">BOOK NOW</button>
-                </a>
-              </li>
-            </ul>
+    <!-- <div class="container-lg">
+      <nav class="navbar navbar-expand-lg ps-1">
+          <div class="my-3 fw-semibold d-flex justify-content-between">
+  
+            
+            <a class="navbar-brand" href="#">
+              <img src="/src/assets/img/header/avada-drivers-logo-1x.webp" alt="">
+            </a>
+  
+          
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button> 
+            <div class="collapse navbar-collapse pt-3 " id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item mx-3 ms-5">
+                  <a class="nav-link active text-white" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item mx-3">
+                  <a class="nav-link text-white" href="../pages/PageAbout.vue">ABOUT</a>
+                </li>
+                <li class="nav-item mx-3">
+                  <a class="nav-link text-white" href="#">PRICES</a>
+                </li>
+                <li class="nav-item dropdown mx-3">
+                  <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    COURSES
+                    <span class="rounded-1 p-1 mx-1 btn span-nav bg-new fw-semibold text-white">NEW</span>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Intensive</a></li>
+                    <li><a class="dropdown-item" href="#">Pass Plus</a></li>
+                    <li><a class="dropdown-item" href="#">Instructor</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item mx-3">
+                  <a class="nav-link text-white" href="#">LOCATIONS</a>
+                </li>
+                <li class="nav-item mx-3">
+                  <a class="nav-link text-white" href="#">BLOG</a>
+                </li>
+                <li class="nav-item mx-3">
+                  <a class="nav-link" href="/src/pages/PageForm.vue">
+                      <button class="btn bg-new rounded-5 px-4 py-2 fw-semibold text-white" type="submit">BOOK NOW</button>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+    </div> -->
+    <div class="container-lg text-uppercase fw-semibold team-10-text-white">
+      <div class="row align-items-center">
+        <div class="col">
+          <img class="py-4" src="../assets/img/header/avada-drivers-logo-1x.webp" alt="driving avada logo">
+        </div>
+        <div class="col">
+          <div class="d-flex justify-content-between align-items-center">
+            <div>home</div>
+            <div>about</div>
+            <div>prices</div>
+            <div>
+              <button class="btn btn-dark dropdown-toggle text-uppercase" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </div>
+            <div>locations</div>
+            <div>blog</div>
+            <div>book now</div>
           </div>
         </div>
-      </nav>
+      </div>
+    </div>
 </header>
 </template>
 
