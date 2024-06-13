dc63d33f429a1c9f5624e7967bf68d38dd46f30c
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-06-13 09:10:46 +0200

    [UPD] AppStartLearning

--- src/components/AppStartLearning.vue
+++ src/components/AppStartLearning.vue
@@ -17,25 +17,34 @@ export default {
 </script>
 
 <template>
-<img :src="getImagesPath('background-car.png')" alt="background car">
-<div class="container d-flex justify-content-between">
-    <div>
-        <h1 class="text-dark">Start Learning to Drive Today</h1>
-        <p class="text-dark">Give us a call to schedule your first driving lesson</p>
+    <div id="start-learning-component" class="container d-flex justify-content-between">
+        <div>
+            <h1 class="text-dark">Start Learning to Drive Today</h1>
+            <p class="text-dark">Give us a call to schedule your first driving lesson</p>
+        </div>
+        <div>
+            <p class="text-dark fs-2">
+                <font-awesome-icon class="me-3" icon="fa-solid fa-phone"/> 1-800-555-555
+            </p>
+        </div>
     </div>
-    <div>
-        <p class="text-dark fs-2">
-            <font-awesome-icon class="me-3" icon="fa-solid fa-phone"/> 1-800-555-555
-        </p>
-    </div>
-</div>
 </template>
 
 <style lang="scss" scoped>
+#start-learning-component{
+    background-image: url(../assets/img/learning/background-car.png);
+    background-size: cover;
+    background-position: bottom;
+    padding-top: 40px;
+    padding-bottom: 40px;
+}
+
 img{
     height: 265px;
     width: 100%;
     display: block;
     object-fit: cover;
+    position: absolute;
+    z-index: -1;
 }
 </style>
